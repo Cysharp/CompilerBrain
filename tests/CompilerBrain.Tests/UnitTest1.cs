3b89be35d8c7428199c73b3d4d5576dbabfc94cd
neuecc <ils@neue.cc>
2025-11-25 15:41:22 +0900

    test infra

--- tests/CompilerBrain.Tests/UnitTest1.cs
+++ tests/CompilerBrain.Tests/UnitTest1.cs
@@ -1,29 +1,11 @@
-﻿//using System.Text.Json;
-//using Xunit.v3;
+﻿namespace CompilerBrain.Tests;
 
-//namespace CompilerBrain.Tests;
-
-//public class UnitTest1(ITestContextAccessor testContextAccessor)
-//{
-//    [Fact]
-//    public async Task Test1()
-//    {
-//        var memory = new SessionMemory();
-
-//        var id = CSharpMcpServer.Initialize(memory);
-
-//        // open self
-
-//        var currentTest = testContextAccessor.Current;
-//        var asmPath = currentTest.TestAssembly!.AssemblyPath;
-//        // var path = Path.Combine(asmPath, "../../", "../../CompilerBrain.Tests.csproj");
-//        var path = Path.Combine(asmPath, "../../../../../../sandbox/ConsoleApp/ConsoleApp.csproj");
-
-
-
-//        var diagnostics = await CSharpMcpServer.OpenCSharpProject(memory, id, path);
-
-
-//        var json = JsonSerializer.Serialize(diagnostics);
-//    }
-//}
+public class UnitTest1
+{
+    [Test]
+    public async Task SampleTest()
+    {
+        var t = true;
+        await Assert.That(t).IsTrue();
+    }
+}
